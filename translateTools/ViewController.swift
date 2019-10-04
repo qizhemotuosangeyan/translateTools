caabe2447ecaeb194d60f04b28692e5264c3f8d9
qizhemotuosangeyan <414772073@qq.com>
2019-10-04 20:20:18 +0800

    Initial Commit

---
+++ translateTools/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  translateTools
+//
+//  Created by 千千 on 2019/10/4.
+//  Copyright © 2019 千千. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
